0ffea12daf174dfa57661814cb811bfb15186515
Changemin <bkm.change.min@gmail.com>
2019-11-10 13:14:47 +0900

    new Stages

--- Stages.h
+++ Stages.h
@@ -104,49 +104,74 @@ int gamePause() {
 	int selectCursor = 1;
 
 	while (1) {
-		int MouseStatus = isMouseClicked();
-		selectCursor = SelectFromN(MouseStatus, selectCursor, 2);
-		switch (selectCursor)
+		switch (isMouseClicked())
 		{
-		case 1:popUp.images[2].y = 0; break;
-		case 2:popUp.images[2].y = 500; break;
-		}
-		if (MouseStatus == SELECT_BUTTON) {
-			switch (selectCursor) {
-			case 1:popUp.fadeOut(&popUp); break;break;
-			case 2:popUp.fadeOut(&popUp); StartStage(); break;
-			}
+		case LEFT_BUTTON:popUp.images[2].y = 0; goto exit; break;
+		case RIGHT_BUTTON:popUp.images[2].y = 500; popUp.fadeOut(&popUp); StartStage(); break;
 		}
 		popUp.renderAll(&popUp);
+	exit:
+		return 0;
 	}
 }
 
 int SelectGameStage() {
 	ImageLayer imageLayer = DEFAULT_IMAGE_LAYER;
 	imageLayer.initialize(&imageLayer); //초기화
-	Image startPage[3] = {
+	Image startPage[6] = {
 		{"GameSelectPage.bmp", 0, 0},
-		{"thumbnail_avoidObstacleSOLOE.bmp",0,0},
-		{"thumbnail_avoidObstacleDOUBLE.bmp",0,0}
+		{"thumbnail_avoidObstacle1P.bmp",0,0},
+		{"thumbnail_avoidObstacle2P.bmp",0,0},
+		{"thumbnail_avoidStar.bmp",0,0},
+		{"thumbnail_goGoSled.bmp",0,0},
+		{"thumbnail_hitTheMouse.bmp",0,0}
 	};
-	imageLayer.imageCount = 3;
+	imageLayer.imageCount = 6;
 	imageLayer.images = startPage;
 	imageLayer.fadeIn(&imageLayer);
 
 	int selectCursor = 1;
 
 	while (1) {
+		Sleep(100);
 		int MouseStatus = isMouseClicked();
-		selectCursor = SelectFromN(MouseStatus, selectCursor, 2);
+		selectCursor = SelectFromN(MouseStatus, selectCursor, 5);
 		switch (selectCursor)
 		{
 		case 1:
 			imageLayer.images[1].x = 0;
 			imageLayer.images[2].x = 700;
+			imageLayer.images[3].x = 1400;
+			imageLayer.images[4].x = 2100;
+			imageLayer.images[5].x = 2800;
 			break;
 		case 2:
-			imageLayer.images[1].x = -700; 
+			imageLayer.images[1].x = -700;
 			imageLayer.images[2].x = 0;
+			imageLayer.images[3].x = 700;
+			imageLayer.images[4].x = 1400;
+			imageLayer.images[5].x = 2100;
+			break;
+		case 3:
+			imageLayer.images[1].x = -1400;
+			imageLayer.images[2].x = -700;
+			imageLayer.images[3].x = 0;
+			imageLayer.images[4].x = 700;
+			imageLayer.images[5].x = 1400;
+			break;
+		case 4:
+			imageLayer.images[1].x = -2100;
+			imageLayer.images[2].x = -1400;
+			imageLayer.images[3].x = -700;
+			imageLayer.images[4].x = 0;
+			imageLayer.images[5].x = 700;
+			break;
+		case 5:
+			imageLayer.images[1].x = -2800;
+			imageLayer.images[2].x = -2100;
+			imageLayer.images[3].x = -1400;
+			imageLayer.images[4].x = -700;
+			imageLayer.images[5].x = 0;
 			break;
 		}
 		if (MouseStatus == SELECT_BUTTON) {
@@ -159,7 +184,45 @@ int SelectGameStage() {
 		imageLayer.renderAll(&imageLayer);
 	}
 }
-
-int AvoidObstacle_SOLO() {
-
+void printScore(int x, int y, int score, int bestScore) {
+	gotoxy(x, y);
+	printf("┌────────────────────┐\n");
+	printf("│ Your Score : %d\n", score);
+	printf("│ best Score : %d\n", bestScore);
+	printf("└────────────────────┘");
 }
+int AvoidObstacle_1P() {
+	ImageLayer imageLayer = DEFAULT_IMAGE_LAYER;
+	imageLayer.initialize(&imageLayer); //초기화
+	int selectCursor = 1;
+	Image startPage[4] = {
+		{"AvoidObstacle1P_BG.bmp", 0, 0},
+		{"AvoidObstacle1P_char.bmp", 0, 0},
+		{"AvoidObstacle1P_Obstacle_left.bmp", 0, 500},
+		{"AvoidObstacle1P_Obstacle_right.bmp", 0, 800}
+	};
+	imageLayer.imageCount = 4;
+	imageLayer.images = startPage;
+	imageLayer.fadeIn(&imageLayer);
+
+	int score = 0;
+	int BestScore = 100;
+	while (1) {
+		printScore(0, 20, score, BestScore);
+		int MouseStatus = isMouseClicked();
+		selectCursor = SelectFromN(MouseStatus, selectCursor, 2);
+		gotoxy(20, 20);
+		switch (selectCursor)
+		{
+		case 1:imageLayer.images[1].x = 0; break;
+		case 2:imageLayer.images[1].x = 500; break;
+		}
+		if (MouseStatus == SELECT_BUTTON) {
+			imageLayer.fadeOut(&imageLayer);
+			return 0;
+		}
+		imageLayer.renderAll(&imageLayer);
+	}
+	
+}
+
--- main.c
+++ main.c
@@ -1,3 +1,20 @@
+/*
+				  ___  ___ _____
+				  |  \/  ||  _  |
+ _   _  ___   ___ | .  . || | | | _   _  ___   ___
+| | | |/ __| / _ \| |\/| || | | || | | |/ __| / _ \
+| |_| |\__ \|  __/| |  | |\ \_/ /| |_| |\__ \|  __/
+ \__,_||___/ \___|\_|  |_/ \___/  \__,_||___/ \___|
+
+ ______  _   _____  ___       ___  _   _  _    _
+| ___ \| | / /|  \/  |      |_  || | | || |  | |
+| |_/ /| |/ / | .  . |        | || |_| || |  | |
+| ___ \|    \ | |\/| |        | ||  _  || |/\| |
+| |_/ /| |\  \| |  | | _  /\__/ /| | | |\  /\  /
+\____/ \_| \_/\_|  |_/( ) \____/ \_| |_/ \/  \/
+					  |/
+useMOuse는 마우스만을 사용하여서 게임을 플레이 할 수 있도록 만든 게임입니다.
+*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<Windows.h>
@@ -19,26 +36,38 @@ static HINSTANCE hlnst;
 #define SCROLLED 4
 #define SELECT_BUTTON 5
 
-int main() {
+int main() { //main 함수는 여러가지 스테이지들의 관계가 goto문으로 관계되어 있습니다.
 	setDisplaySize(180, 49);
 	enableMouseInput();
 startPage:
-	switch(StartStage()){
+	switch(StartStage()){ // 시작 페이지에서 1(게임 시작)을 선택하면 게임 선택화면으로 , 2(게임 설명)를 선택하면 게임 설명 페이지로 갑니다.
 		case 1:goto selectGame; break;
 		case 2:goto gameExplain; break;
 	}
 gameExplain:
-	switch (GameExplainStage()) {
+	switch (GameExplainStage()) { // 게임 설명 페이지에서 1(게임 시작)을 선택하면 다시 시작 화면으로 , 2(게임 선택)를 선택하면 게임 선택 페이지로 갑니다.
 		case 1:goto startPage; break;
 		case 2:goto selectGame; break;
 	}
 selectGame:
-	switch (SelectGameStage()) {
+	switch (SelectGameStage()) { // 게임 선택 페이지에서 0(되돌아 가기)을 선택하면 게임 선택화면으로 , n(각 게임들)를 선택하면 그 게임이 실행됩니다.
 		case 0:goto startPage; break;
-		case 1:goto avoidObstacle_solo; break;
+		case 1:goto avoidObstacle_1p; break;
+		case 2:goto avoidObstacle_2p; break;
+		case 3:goto avoid_star; break;
+		case 4:goto hit_the_mouse; break;
+		case 5:goto go_go_sled; break;
 	}
-avoidObstacle_solo:
-	switch (AvoidObstacle_SOLO()) {
+avoidObstacle_1p:
+	switch (AvoidObstacle_1P()) { // 게임 플레이 화면에서 0을 선택하면 다시 첫 화면으로 갑니다.
 		case 0: goto startPage; break;
 	}
+avoidObstacle_2p:
+	return 0;
+avoid_star:
+	return 0;
+hit_the_mouse:
+	return 0;
+go_go_sled:
+	return 0;
 }
